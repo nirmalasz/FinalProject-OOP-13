07c46fcd7fd17f5568490cadb088a10356ec18cc
Nirmala <nirmalawilujeng@gmail.com>
2025-12-12 18:51:49 +0700

    Buat awalan GameManager, observer setter getter will be added later

--- frontend/core/src/main/java/com/kelompok13/frontend/utils/GameManager.java
+++ frontend/core/src/main/java/com/kelompok13/frontend/utils/GameManager.java
@@ -1,4 +1,52 @@
 package com.kelompok13.frontend.utils;
 
+import com.badlogic.gdx.Gdx;
+
 public class GameManager {
+    private static GameManager instance;
+    private boolean gameActive;
+    //private ScoreManager scoreManager;
+    // private BackendService backendService; // u req api
+    private String currentPlayerId; //untuk uuid
+    private int currencyCollected = 0;
+
+    private GameManager(){
+        //scoreManager = new ScoreManager();
+        gameActive = false;
+        //backendService = new BackendService();
+    }
+
+    public static GameManager getInstance(){
+        if (instance == null){
+            instance = new GameManager();
+        }
+        return instance;
+    }
+
+    public void registerPlayer(String username){
+
+    }
+
+    public void startGame(){
+        gameActive = true;
+        System.out.println("Game Started!");
+        currencyCollected = 0;
+    }
+
+    public void endGame(){
+
+    }
+
+    public void addCurrency(int value){
+        currencyCollected += value;
+        Gdx.app.log("Currency Collected", "Total: " + currencyCollected );
+    }
+
+    //setter getter tp ntar aj
+    //buat score
+    //buat currency
+
+    //buat observer
+        
+
 }
