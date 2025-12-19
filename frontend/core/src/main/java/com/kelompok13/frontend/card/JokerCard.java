bea5ffbaff533a46e5878ce449fc9c9ce35179f6
Nirmala <nirmalawilujeng@gmail.com>
2025-12-20 00:27:43 +0700

    Add apply effect and effect attributes to joker class

--- frontend/core/src/main/java/com/kelompok13/frontend/card/JokerCard.java
+++ frontend/core/src/main/java/com/kelompok13/frontend/card/JokerCard.java
@@ -1,10 +1,28 @@
 //special card with unique effects
 package com.kelompok13.frontend.card;
 
+import com.kelompok13.frontend.card.effects.CardEffect;
+
 public class JokerCard extends BaseCard{
-    public JokerCard(){
+    private CardEffect effect;
+
+    public JokerCard(CardEffect effect){
         super("Joker", CardType.JOKER);
+        this.effect = effect;
     }
 
+    public CardEffect getEffect(){
+        return effect;
+    }
 
+    public void setEffect(CardEffect effect){
+        this.effect = effect;
+    }
+
+    public int applyEffect(String type, int value){
+        if (effect != null){
+            return effect.applyEffect(type, value);
+        }
+        return value;
+    }
 }
