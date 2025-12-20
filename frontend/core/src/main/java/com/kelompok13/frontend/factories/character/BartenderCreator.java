94968dc96bfe3beb680d6f4230694d1057dd089d
Nirmala <nirmalawilujeng@gmail.com>
2025-12-20 14:20:57 +0700

    bartender creator

--- frontend/core/src/main/java/com/kelompok13/frontend/factories/character/BartenderCreator.java
+++ frontend/core/src/main/java/com/kelompok13/frontend/factories/character/BartenderCreator.java
@@ -1,10 +1,16 @@
 package com.kelompok13.frontend.factories.character;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.Vector2;
 import com.kelompok13.frontend.characters.Bartender;
 import com.kelompok13.frontend.characters.BaseCharacter;
 
 public class BartenderCreator {
-
+//placeholder
+    Texture texture = new Texture("characters/bartender.png");
     public BaseCharacter createCharacter() {
-        return new Bartender();
+        String[] dialogues = {
+           //
+        };
+        return new Bartender("NamaBartender", new Vector2(10, 10),  dialogues, texture);
     }
 }
